b8c50064a6f4c2e0dfdd36c2e4ecd3bf7afd51e0
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-10-13 07:27:14 +0700

    feat: initial page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@auth', replacement: '/src/auth' },
+    ]
+  }
 })
