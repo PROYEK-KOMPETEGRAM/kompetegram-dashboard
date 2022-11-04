687be3f933ee1a2efe249a5d0fc341ea3fa7d649
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-11-04 22:02:27 +0700

    feat: added donut chart base component

--- src/analytics/pages/AnalyticsPage.tsx
+++ src/analytics/pages/AnalyticsPage.tsx
@@ -7,6 +7,57 @@ import { getDepartmentsCount, getMajorsCount, getMembersCount, getVerifiedMember
 import { useEffect } from "react";
 import { Modals } from "@/commons/components/Modals/Modals";
 import { useRef } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const chartData = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2],
+      backgroundColor: [
+        '#1E40AF',
+        '#1D4ED8',
+        '#2563EB',
+        '#3B82F6',
+        '#60A5FA',
+      ],
+      borderColor: [
+        '#60A5FA',
+        '#60A5FA',
+        '#60A5FA',
+        '#60A5FA',
+        '#60A5FA',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const legendMargin = {
+  id: "legendMargin",
+  beforeInit: (chart: any) => {
+    const fitValue = chart.legend.fit;
+
+    chart.legend.fit = function fit() {
+      fitValue.bind(chart.legend)();
+      return (this.height += 40);
+    };
+  }
+};
+
+const chartOptions = {
+  plugins: {
+    legend: {
+      labels: {
+        color: 'white'
+      }
+    }
+  }
+}
 
 export const AnalyticsPage = () => {
   const [membersCount, setMembersCount] = useState<string | number>(0);
@@ -111,6 +162,32 @@ export const AnalyticsPage = () => {
             <Stats value={departmentsCount} title="Jumlah Fakultas" >
               <FaBuilding color="#64b5f6" size="1.25em" />
             </Stats>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="bg-gray-700 shadow-sm rounded-lg lg:my-8 sm:my-4 my-2">
+          <h3 
+            className="p-4 font-open-sans text-gray-100 font-medium text-sm sm:text-base my-1 uppercase"
+          >
+            Program Studi Terbanyak - Top 5
+          </h3>
+          <div 
+            className="lg:px-16 lg:pb-16 lg:pt-6 sm:px-8 sm:pb-8 sm:pt-4 px-6 pb-8 pt-2"
+          >
+            <Doughnut data={chartData} options={chartOptions} plugins={[legendMargin]} />
+          </div>
+        </div>
+        <div className="bg-gray-700 shadow-sm rounded-lg lg:my-8 sm:my-4 my-2">
+          <h3 
+            className="p-4 font-open-sans text-gray-100 font-medium text-sm sm:text-base my-1 uppercase"
+          >
+            Fakultas Terbanyak - Top 5
+          </h3>
+          <div 
+            className="lg:px-16 lg:pb-16 lg:pt-6 sm:px-8 sm:pb-8 sm:pt-4 px-6 pb-8 pt-2"
+          >
+            <Doughnut data={chartData} options={chartOptions} plugins={[legendMargin]} />
           </div>
         </div>
       </div>
